b55b771999cbc0562715de229983114c1778695b
Rose McClain <rosemcclain024@gmail.com>
2019-03-07 07:03:07 -0500

    audio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,16 @@
 // YAY PSUEDO CODING !!
-//Create a start button to initialize the start of the game and timer.
-// Need to create a countdown timer counting down from (??).
-// When the timer hits zero the game needs to reset.
+//Create a start button to initialize the start of the game and timer. When you press the button you should have 32 seconds to answer the questions. (as long as the song is playing)
+// Need to create a countdown timer counting down from (32 seconds).
 // When the timer hits zero the scores will be updated. 
 // When the timer hits zero the user will be notified that times up!
-// When creating the questions their can only be one answer.
-// Create a button to calculate the scores. This will have the same outcome as when the timer hits zero.
+// When creating the questions their can only be one answer for each question.
+// Create a button to calculate the scores (done, used if the user finishes early).(This will have the same outcome as when the timer hits zero.)
 //
 
 
 $(document).ready(function() {
 
-    
+    $(".btn btn-outline-dark").on("click")
 
 
 
