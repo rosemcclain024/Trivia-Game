7afc381f3d0a20c864ec7b609a82e0d84505060b
Rose McClain <rosemcclain024@gmail.com>
2019-03-07 06:06:57 -0500

    html and some css

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// YAY PSUEDO CODING !!
+//Create a start button to initialize the start of the game and timer.
+// Need to create a countdown timer counting down from (??).
+// When the timer hits zero the game needs to reset.
+// When the timer hits zero the scores will be updated. 
+// When the timer hits zero the user will be notified that times up!
+// When creating the questions their can only be one answer.
+// Create a button to calculate the scores. This will have the same outcome as when the timer hits zero.
+//
+
+
+$(document).ready(function() {
+
+    
+
+
+
+
+
+})
